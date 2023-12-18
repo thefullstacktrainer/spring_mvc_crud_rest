eb1ab193575f078ef0700ed7f2a281ce376bc54d
Lakshmikant <fullstacktrainer4u@gmail.com>
2023-12-18 16:31:53 +0530

    Basic maven setup

---
+++ src/main/java/com/lucky/HelloController.java
@@ -0,0 +1,13 @@
+package com.lucky;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+	
+	@RequestMapping("/")
+	public String display() {
+		return "index";
+	}
+}
