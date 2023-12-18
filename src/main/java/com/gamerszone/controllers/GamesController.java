e8ec6163f412fdc52ab3c63b6100b5a03b10c99c
Lakshmikant <fullstacktrainer4u@gmail.com>
2023-12-19 01:20:14 +0530

    Combined all into GamesController

--- src/main/java/com/gamerszone/controllers/GamesController.java
+++ src/main/java/com/gamerszone/controllers/GamesController.java
@@ -1,16 +1,23 @@
-// src/main/java/com/gamerszone/controllers/GamesController.java
+// src/main/java/com/gamerszone/controllers/GameController.java
 
 package com.gamerszone.controllers;
-
-import java.util.Arrays;
-import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class GamesController {
+
+    @GetMapping("/addGame")
+    public String showAddGameForm() {
+        return "addGameForm";
+    }
 
     @GetMapping("/games")
     public String showAllGames(Model model) {
@@ -23,5 +30,15 @@ public class GamesController {
 
         // Return the view name
         return "allGames";
+    }
+
+    @PostMapping("/saveGame")
+    public String saveGame(@RequestParam String gameName) {
+        // Logic to save the game to the database or service
+        // For simplicity, let's print the game name for now
+        System.out.println("Saving Game: " + gameName);
+
+        // Redirect to the /games endpoint to show the updated list of games
+        return "redirect:/games";
     }
 }
